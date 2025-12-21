d26d5ccc631e3a17d2d4c93c1e3d315a68379e1e
Stephen1393 <stephen.bending20@gmail.com>
2025-12-21 16:02:12 +0000

    Wrote another end-point test for turtle. Linked the function sayHello to the route.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { sayHello } = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
-  res.status(200).json({ result: `Hello, ${req.params.string}!` });
+  const { string } = req.params;
+  const result = sayHello(string);
+  res.status(200).json({ result });
 });
 
 module.exports = app;
